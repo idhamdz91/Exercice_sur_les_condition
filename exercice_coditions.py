c455e8c78a280a2bdf9883b424b92b400fa2ecc3
idhamdz91 <naoui.mahdi@yahoo.fr>
2025-01-05 19:52:02 +0100

    Create exercice_coditions.py

---
+++ exercice_coditions.py
@@ -0,0 +1,33 @@
+note1 = float(input("veuillez introdure la note 01: "))
+
+note2 = float(input("veillez introduire la note 02: "))
+
+note=[note1, note2,]
+
+moyenne = sum(note)/len(note)
+
+if moyenne >= 10:
+
+    print ("la moyenne est de : ", moyenne, "Félicitation !")
+
+else :
+
+        print (moyenne)
+
+ 
+
+ 
+
+x = float(input("Veuiller introduire un nombre: "))
+
+resulat = 1/x
+
+if x != 0:
+
+    print(resulat)
+
+else:
+
+    print ("l'inversse n'existe pas pour 0 ")
+
+ 
